b1231f2347aec62d7a54c06a09748836bd9d61ef
Jenna <jwz5299@uw.edu>
2025-01-02 11:59:44 +0800

    changed blogging views to classes

--- blogging/views.py
+++ blogging/views.py
@@ -3,23 +3,31 @@ from django.shortcuts import render
 from django.template import loader
 from blogging.models import Post
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    body = template.render(context)
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
 
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     body = template.render(context)
+#     return render(request, 'blogging/list.html', context)
 
-def detail_view(request,post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    #render completes template loading, creating context, generating page body, and making HTTP response at once
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+# def detail_view(request,post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     #render completes template loading, creating context, generating page body, and making HTTP response at once
+#     return render(request, 'blogging/detail.html', context)
