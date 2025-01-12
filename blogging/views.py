3c5b2e4eeda49345236c397e02b15fb6dac4e8f7
Jenna <jwz5299@uw.edu>
2025-01-12 04:02:15 -0800

    cleaned up comments

--- blogging/views.py
+++ blogging/views.py
@@ -13,26 +13,6 @@ class BlogListView(ListView):
     template_name = "blogging/list.html"
 
 
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by('-published_date')
-#     template = loader.get_template('blogging/list.html')
-#     context = {'posts': posts}
-#     body = template.render(context)
-#     return render(request, 'blogging/list.html', context)
-
-
 class BlogDetailView(DetailView):
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
-
-
-# def detail_view(request,post_id):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     try:
-#         post = published.get(pk=post_id)
-#     except Post.DoesNotExist:
-#         raise Http404
-#     context = {'post': post}
-#     #render completes template loading, creating context, generating page body, and making HTTP response at once
-#     return render(request, 'blogging/detail.html', context)
