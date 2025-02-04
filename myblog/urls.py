a5404cde6f658f4afd7bdb35530b8735195412d3
Jenna <jwz5299@uw.edu>
2025-02-03 21:56:31 -0800

    completed following tutorial

--- myblog/urls.py
+++ myblog/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("polling/", include("polling.urls")),
     path("", include("blogging.urls")),
-    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path("accounts/", include("allauth.urls")),
+    # path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    # path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
