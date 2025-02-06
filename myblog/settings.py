1933540b5581efc98e0a2ffc18786f4d9006415a
Jenna <jwz5299@uw.edu>
2025-02-06 01:13:08 -0800

    github login added line defining socialaccount

--- myblog/settings.py
+++ myblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     "allauth",
     "allauth.account",
+    "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
     "polling",
     "blogging",
